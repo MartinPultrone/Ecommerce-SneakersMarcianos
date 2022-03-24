0126484386b68a96946c82574d872632bd7618ca
Martin Pultrone <pmartinpultrone@gmail.com>
2022-03-24 20:20:32 -0300

    Clase 11

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,10 +1,15 @@
+import { memo } from 'react'
 import Item from "../Item/Item"
 
+// memo( ()=>{} ) es el componente // memo( ()=>{}, fn ) es el componente
+const ItemList = memo(
+    ({productos}) => {
+        console.log('ItemList')
+        return (
+        productos.map((prod) => <Item key={prod.id} prod={prod} /> )
+        )
+    }
+, (oldProps, newProps) => oldProps.productos.length === newProps.productos.length)
 
-const ItemList = ({ productos }) => {
-    return (
-        productos.map((prod) => <Item key={prod.id} prod={prod} />)
-    )
-}
 
 export default ItemList
